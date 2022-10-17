bb1f7a10010ca95ddae808aa6f97cb5a36e18910
raquelsscode <raquelsantossilva2405@outlook.com>
2022-10-17 19:36:41 -0300

    feat: desafio 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,5 @@
+// Retorne o nome de todos os lanches que possuam calorias abaixo de 500
+
+db.produtos.find({ 
+    valoresNutricionais: { $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } },
+    { _id: 0, nome: 1 });
