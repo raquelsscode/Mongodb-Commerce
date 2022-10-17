30f09cd769e6e39748d9021d3f7875f25598369a
raquelsscode <raquelsantossilva2405@outlook.com>
2022-10-17 18:00:13 -0300

    feat: desafio 6

---
+++ challenges/desafio6.js
@@ -0,0 +1,5 @@
+// Retorne o nome e as curtidas dos lanches que tiveram
+// curtidas maiores que 10 e menores que 100
+
+db.produtos.find({ $and: [{ curtidas: { $gt: 10 } }, { curtidas: { $lt: 100 } }] },
+    { _id: 0, nome: 1, curtidas: 1 });
